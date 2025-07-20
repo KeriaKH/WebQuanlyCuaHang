67017faf5001cc733792463c82c3765203fbb299
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2025-07-20 22:22:51 +0700

    ...

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,37 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute=require('./routes/auth')
-const userRoute=require('./routes/user')
-const dishRoute=require('./routes/dish')
-const categoryRoute=require('./routes/category')
-const reviewRoute=require('./routes/review')
-const voucherRoute=require('./routes/voucher')
-const orderRoute=require('./routes/order')
-const cloudinaryRoute=require('./routes/cloudinary')
+dotenv.config();
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const dishRoute = require("./routes/dish");
+const categoryRoute = require("./routes/category");
+const reviewRoute = require("./routes/review");
+const voucherRoute = require("./routes/voucher");
+const orderRoute = require("./routes/order");
+const cloudinaryRoute = require("./routes/cloudinary");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'https://web-quanly-cua-hang-ead3.vercel.app', 
-  credentials: true                
-}));
+app.use(
+  cors({
+    origin: "https://web-quanly-cua-hang-ead3.vercel.app",
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoute);
-app.use('/api/user',userRoute)
-app.use('/api/dish',dishRoute)
-app.use('/api/category',categoryRoute)
-app.use('/api/review',reviewRoute)
-app.use('/api/voucher',voucherRoute)
-app.use('/api/order',orderRoute)
-app.use('/api/cloudinary',cloudinaryRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/dish", dishRoute);
+app.use("/api/category", categoryRoute);
+app.use("/api/review", reviewRoute);
+app.use("/api/voucher", voucherRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/cloudinary", cloudinaryRoute);
 
 const Port = process.env.PORT || 3000;
 
@@ -37,7 +41,7 @@ mongoose
     console.log("✅ Connected to MongoDB");
     app.listen(Port, () => {
       console.log(`🚀 Server running on http://localhost:${Port}`);
-      console.log('🔗 Connected URI:', process.env.MONGO_URI);
+      console.log("🔗 Connected URI:", process.env.MONGO_URI);
     });
   })
   .catch((err) => {
