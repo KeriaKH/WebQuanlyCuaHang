3cd7b34900e48c809653b899633d355f4cd5e0d4
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2025-07-20 22:29:13 +0700

    ...

--- backend/cloudinary/cloudinary.js
+++ backend/cloudinary/cloudinary.js
@@ -1,10 +1,5 @@
 const cloudinary = require("cloudinary").v2;
 
-
-console.log('🔍 Environment variables:');
-console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
-console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET ? '***hidden***' : 'undefined');
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
