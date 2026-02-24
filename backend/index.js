04ae1a70d725090b0880b6c12d6d7203c73603bb
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2026-02-24 13:09:07 +0700

    fix: update CORS origin and API URL for production environment

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost",
+    origin: "web-quanly-cua-hang-ead3.vercel.app",
     credentials: true,
   })
 );
